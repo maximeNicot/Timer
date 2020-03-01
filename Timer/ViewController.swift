ff746b69a90889f28ceed51e45ba825a3da70ddf
Antoine NICOT <Antoine@macbook-air-de-antoine.local>
2020-03-01 17:35:55 +0100

    timer

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,13 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var quickStart: UIButton!
+    @IBOutlet weak var labelTimer: UILabel!
 
+    var timer = NSTimer()
+    var chrono : Int = 0
+    var oneTimer = true
+    
     @IBAction func quickStartAction(sender: AnyObject) {
          print("Hello world")
+        
+        if(oneTimer){
+        self.timer =  NSTimer.scheduledTimerWithTimeInterval(1.0,target: self, selector: Selector("incrementer"), userInfo: nil, repeats: true)
+            oneTimer = false
+        }
+        
     }
+    
+    @IBOutlet weak var pause: UIButton!
+    @IBAction func pause(sender: AnyObject) {
+        oneTimer = true
+        timer.invalidate()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        chrono = 0
+        labelTimer.text = String(chrono)
+        oneTimer = true
+        timer.invalidate()
+    }
+    
+    internal func incrementer(){
+        chrono += 1
+        labelTimer.text = "\(chrono)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,6 +57,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   
 
 }
 
