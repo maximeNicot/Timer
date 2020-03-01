190343623a7afc4b50d8c95a1b2528e0a5acd83a
Antoine NICOT <Antoine@macbook-air-de-antoine.local>
2020-03-01 14:20:18 +0100

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Antoine NICOT on 01/03/2020.
+//  Copyright © 2020 maxime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
